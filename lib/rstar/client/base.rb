0220524152ed25d492f482deb87242cf68b9dcbe
Joseph Pawletko <jgpawletko@gmail.com>
2014-02-16 20:13:12 -0500

    tweaked base resource to use generic, non-extension specific URLs

--- lib/rstar/client/base.rb
+++ lib/rstar/client/base.rb
@@ -3,9 +3,21 @@ require 'active_resource'
 module Rstar
   module Client
     class BaseResource < ::ActiveResource::Base
+      class << self
+        def element_path(id, prefix_options = {}, query_options = nil)
+          prefix_options, query_options = split_options(prefix_options) if query_options.nil?
+          "#{prefix(prefix_options)}#{collection_name}/#{id}#{query_string(query_options)}"
+        end
+        def collection_path(prefix_options = {}, query_options = nil)
+          prefix_options, query_options = split_options(prefix_options) if query_options.nil?
+          "#{prefix(prefix_options)}#{collection_name}#{query_string(query_options)}"
+        end
+      end
+
       self.site     = ENV['RSTAR_URL']
       self.user     = ENV['RSTAR_HTTP_USER']
       self.password = ENV['RSTAR_HTTP_PASSWORD']
+      self.prefix   = '/api/v0/'
     end
   end
 end
--- lib/rstar/client/version.rb
+++ lib/rstar/client/version.rb
@@ -1,5 +1,5 @@
 module Rstar
   module Client
-    VERSION = "0.0.1.alpha.001"
+    VERSION = "0.0.1.alpha.002"
   end
 end
