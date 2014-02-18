954096d113a5f307005516bf50c4461ff555b25d
Joseph Pawletko <jgpawletko@gmail.com>
2014-02-18 14:51:30 -0500

    added vclips

---
+++ lib/vstructapi/client/vclip.rb
@@ -0,0 +1,7 @@
+module Vstructapi
+  module Client
+    class Vclip < Vstructapi::Client::Base
+      self.element_name = "vclip"
+    end
+  end
+end
--- lib/vstructapi/client/vstruct.rb
+++ lib/vstructapi/client/vstruct.rb
@@ -2,6 +2,8 @@ module Vstructapi
   module Client
     class Vstruct < Vstructapi::Client::Base
       self.element_name = "vstruct"
+
+      has_many :vclips
     end
   end
 end
